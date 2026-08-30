7913c5dad4d608bb4151ba75237b4bb46e2d1b16
yasir <aloufiyasir@gmail.com>
2026-08-31 02:43:47 +0300

    removed "refrences"

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -25,7 +25,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 				content: "width=device-width, initial-scale=1",
 			},
 			{
-				title: "Index - Find KAU Courses & Instructors",
+				title: "Index - Find Courses & Instructors",
 			},
 			{
 				name: "description",
